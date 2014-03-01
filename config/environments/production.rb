458b65cbf445b0ee23d7b36a4be42365f9a3f104
rememberlenny <leonard_bogdonoff@condenast.com>
2014-02-28 19:21:04 -0500

    Fix the config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Wcic::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  onfig.ember.variant = :production
+  config.ember.variant = :production
 end
